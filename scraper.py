33959503e15985e3aa5333bd415cbbf70d4b7c76
woodbine <ian@spendnetwork.com>
2016-12-09 16:13:34 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -83,7 +83,7 @@ def convert_mth_strings ( mth_string ):
 #### VARIABLES 1.0
 
 entity_id = "E5020_THLBC_gov"
-url = "http://www.towerhamlets.gov.uk/lgnl/council_and_democracy/council_budgets_and_spending/transparency/payments_to_suppliers.aspx"
+url = "http://www.towerhamlets.gov.uk/lgnl/council_and_democracy/Transparency/payments_to_suppliers.aspx"
 errors = 0
 data = []
 
@@ -94,34 +94,32 @@ soup = BeautifulSoup(html, 'lxml')
 
 
 #### SCRAPE DATA
-import requests
 
-html = requests.get(url)
-soup = BeautifulSoup(html.text, 'lxml')
-
-links = soup.findAll('a', href=True)
-for link in links:
-    url = 'http://www.towerhamlets.gov.uk/' + link['href']
-    title = link.text.strip()
-    if title.startswith('Payment f') or title.startswith('Payments f') and 'card' not in title:
-        title = title.upper().replace('\r',' ').replace('\n',' ')
-        csvYr = title.replace(u'\xa0', ' ').strip().split(' ')[-1]
-        csvMth = title.replace(u'\xa0', ' ').split(' ')[-2][:3]
-        if 'APRIL' in title.replace(u'\xa0', ' ').split(' ')[-3]:
-            csvMth = title.replace(u'\xa0', ' ').split(' ')[-3][:3]
-        csvMth = convert_mth_strings(csvMth.upper())
-        data.append([csvYr, csvMth, url])
-table_links = soup.find('table').find('tbody').find_all('tr')
+table_links = soup.find_all('a', 'sys_15')
 for table_link in table_links:
-    title = table_link.find('th').text
-    csvYr = title.strip().split('for ')[-1].split(' ')[-1]
-    csvMth = title.strip().split('for ')[-1].split(' ')[0][:3]
-    try:
-        url = 'http://www.towerhamlets.gov.uk/' + table_link.find('td').find_next('td').find('a')['href']
-    except:
-        pass
+    title =table_link.find_previous('th').text
+    url = 'http://www.towerhamlets.gov.uk/' + table_link['href']
+    csvYr = title[-4:]
+    csvMth = title[:3]
+    if 'AHW-Directorate' in url:
+        break
+    if 'spend' in title:
+        csvYr = title.split()[1]
     csvMth = convert_mth_strings(csvMth.upper())
     data.append([csvYr, csvMth, url])
+table_links = soup.find_all('a', 'sys_18')
+for table_link in table_links:
+    title = table_link.text.replace(u'\xa0', ' ')
+    if 'Payment' in title:
+
+        csvYr = title.split('for ')[-1].strip()[-4:]
+        csvMth = title.split('for ')[-1].strip()[:3]
+        try:
+            url = 'http://www.towerhamlets.gov.uk/' + table_link['href']
+        except:
+            pass
+        csvMth = convert_mth_strings(csvMth.upper())
+        data.append([csvYr, csvMth, url])
 
 
 
